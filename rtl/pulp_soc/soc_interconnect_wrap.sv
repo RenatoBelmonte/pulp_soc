7bbfe85dec1423a4b93b64d7ecc64b1ec2996b96
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-19 09:34:05 +0200

    Corrected linking

--- rtl/pulp_soc/soc_interconnect_wrap.sv
+++ rtl/pulp_soc/soc_interconnect_wrap.sv
@@ -197,7 +197,7 @@ module soc_interconnect_wrap
 
   `AXI_ASSIGN(axi_slave_plug, axi_slaves[0])
   `AXI_ASSIGN(axi_to_axi_lite_bridge, axi_slaves[1])
-  `AXI_ASSIGN(wide_alu_slave, axi_slaves[2])
+  `AXI_ASSIGN(spiker_adapter_slave, axi_slaves[2])
 
   //Interconnect instantiation
   soc_interconnect #(
